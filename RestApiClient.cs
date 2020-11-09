b1d24e0836f74c51a46c2baaea5dbccfe2004594
DimaKutko <dimakutko410@gmail.com>
2020-11-09 12:43:51 +0200

    Added endepoints

---
+++ RestApiClient.cs
@@ -0,0 +1,10 @@
+﻿class RestApiClient
+{
+    private static readonly string api = "http://www.omdbapi.com/?";
+    private static readonly string apiKey = "apikey=fdb692c9";
+    private static readonly string optionsTitile = "t=";
+    private static readonly string optionsID = "i=";
+    private static readonly string optionsSearch = "s=";
+    private static readonly string optionsTtype = "type=movie";
+    private static readonly string optionsDataType = "r=xml";
+}
