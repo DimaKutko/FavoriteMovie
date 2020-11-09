bc6ba760d7a456537d6117fdd7e2953bd5de8b1d
DimaKutko <dimakutko410@gmail.com>
2020-11-09 12:44:27 +0200

    add year

--- RestApiClient.cs
+++ RestApiClient.cs
@@ -4,7 +4,10 @@
     private static readonly string apiKey = "apikey=fdb692c9";
     private static readonly string optionsTitile = "t=";
     private static readonly string optionsID = "i=";
+    private static readonly string optionsYear = "y=";
     private static readonly string optionsSearch = "s=";
     private static readonly string optionsTtype = "type=movie";
     private static readonly string optionsDataType = "r=xml";
+
+
 }
