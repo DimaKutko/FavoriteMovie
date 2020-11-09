9848e698ff9937b54a1a647b61d547afb94cb668
DimaKutko <dimakutko410@gmail.com>
2020-11-09 12:03:01 +0200

    Update Movie.cs

--- Movie.cs
+++ Movie.cs
@@ -1,4 +1,51 @@
-﻿public class Movie
-{
+﻿using System;
 
+public abstract class Movie
+{
+    protected String title;
+    protected int year;
+
+    protected Movie(String title, int year)
+    {
+        this.title = title;
+        this.year = year;
+    }
+
+    protected Movie(String title, String year)
+    {
+        try
+        {
+            this.title = title;
+            this.year = Convert.ToInt32(year);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error {e.Message}");
+        }
+      
+    }
+}
+
+public class FullMovie : Movie{
+    private bool viewed;
+    private int rating;
+    private String comment;
+
+    private String writer;
+    private String genre;
+    private String actors;
+    private String plot;
+
+    public FullMovie(String title, String year) : base(title, year) {
+
+    }
+}
+
+public class ShortMovie : Movie {
+    private String imdbID;
+
+    public ShortMovie(String title, String year, String imdbID) : base(title, year)
+    {
+        this.imdbID = imdbID;
+    }
 }
